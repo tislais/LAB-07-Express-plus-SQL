711142775eeb7aa249ac785d19fddf5d7ba4b08c
Tis Lais <tistististis@gmail.com>
2021-05-07 09:13:39 -0700

    uncomment last test and comment another

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -106,17 +106,17 @@ describe('API Routes', () => {
     expect(response.body).toEqual(expect.arrayContaining([scaredStiff, indianaJones, theAddamsFamily]));
   });
 
-  // it('GET theAddamsFamily from /api/machines/:id', async () => {
-  //   const response = await request.get(`/api/machines/${theAddamsFamily.id}`);
-  //   expect(response.status).toBe(200);
-  //   expect(response.body).toEqual(theAddamsFamily);
-  // });
-
-  it('GET by title from /api/machines/titles/', async () => {
-    const response = await request.get(`/api/machines/titles/${theAddamsFamily.title}`);
+  it('GET theAddamsFamily from /api/machines/:id', async () => {
+    const response = await request.get(`/api/machines/${theAddamsFamily.id}`);
     expect(response.status).toBe(200);
     expect(response.body).toEqual(theAddamsFamily);
   });
+
+  // it('GET by title from /api/machines/titles/', async () => {
+  //   const response = await request.get(`/api/machines/titles/${theAddamsFamily.title}`);
+  //   expect(response.status).toBe(200);
+  //   expect(response.body).toEqual(theAddamsFamily);
+  // });
 
   // it('DELETE theAddamsFamily from /api/machines/:id', async () => {
   //   const response = await request.delete(`/api/machines/${theAddamsFamily.id}`);
